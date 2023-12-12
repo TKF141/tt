4abf1a9971100ea5a8cb5dc5d37b876e667605ea
TKF141 <143975705+TKF141@users.noreply.github.com>
2023-12-12 10:34:17 +0900

    Add files via upload

--- tt.py
+++ tt.py
@@ -1,19 +1,28 @@
-age = 20
-if age < 20:
-    print('aa')
-else:
-    print('bb')
-'apple' + 'grape'
-'apple' * 3
+import streamlit as st
+import matplotlib.pyplot as plt
+# import numpy as np
 
-def user_sum(n):
-    s = 0
-    for i in range(1, n+1):
-        s = s + 1
-    s
+love = [] 
+x = []
+for i in range(-29,30,7):
+    love.append(i)
+    x.append(-2*i*i+3*i+5)
 
-user_sum(100)
-user_sum(200)
+a = st.number_input('a의 값을 입력하시오', value=2.0, step=1.0)
+b = st.number_input('b의 값을 입력하시오', value=-1.0, step=1.0)
+c = st.number_input('c의 값을 입력하시오', value=15.0, step=1.0)
+d = st.number_input('d의 값을 입력하시오', value=2000.0, step=100.0)
+
+c1 = st.sidebar.radio('선의 색을 선택하시오',['red','green','blue','orange'])
+s1 = st.sidebar.radio('선의 형태를 선택하시오',['-',':','-.','--'])
+m1 = st.sidebar.radio('마커의 형태를 선택하시오',['o','^','s','p'])
+fig, ax = plt.subplots()
+
+plt.plot(love,x,color=c1, linestyle=s1, marker=m1)
+st.pyplot(fig)
+
+import sys
+sys.exit()
 
 s = 0
 for i in range(10, 21):
@@ -41,7 +50,7 @@ s1 = st.sidebar.radio('선의 형태를 선택하시오',['-',':','-.','--'])
 m1 = st.sidebar.radio('마커의 스타일을 선택하시오',['o','^','s','p'])
 fig, ax = plt.subplots()
 
-love = []
+love = [] 
 y = []
 for i in range(-20,21,2):
     love.append(i)
@@ -55,4 +64,28 @@ sys.exit()
 
 fig, ax = plt.subplots()
 
+import streamlit as st
+import matplotlib.pyplot as plt
+# import numpy as np
 
+love = [] 
+x = []
+for i in range(-29,5,28):
+    love.append(i)
+    x.append(-2*i*i+3*i+5)
+
+a = st.number_input('a의 값을 입력하시오', value=2.0, step=1.0)
+b = st.number_input('b의 값을 입력하시오', value=-1.0, step=1.0)
+c = st.number_input('c의 값을 입력하시오', value=15.0, step=1.0)
+d = st.number_input('d의 값을 입력하시오', value=2000.0, step=100.0)
+
+c1 = st.sidebar.radio('선의 색을 선택하시오',['red','green','blue','orange'])
+s1 = st.sidebar.radio('선의 형태를 선택하시오',['-',':','-.','--'])
+m1 = st.sidebar.radio('마커의 형태를 선택하시오',['o','^','s','p'])
+fig, ax = plt.subplots()
+
+plt.plot(love,y,color=c1, linestyle=s1, marker=m1)
+st.pyplot(fig)
+
+import sys
+sys.exit()
